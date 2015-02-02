62486f835f6cd9b3bed8688aade90e7beb5ed9f1
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-02 19:07:40 +0000

    added SoilGrid.validator.R

--- pkg/R/spsample.prob.R
+++ pkg/R/spsample.prob.R
@@ -1,4 +1,4 @@
-# Purpose        : estimate inclusion probabilities;
+# Purpose        : estimate occurrence probabilities;
 # Maintainer     : Tomislav Hengl (tom.hengl@wur.nl);
 # Contributions  : ;
 # Status         : pre-alpha
@@ -29,7 +29,7 @@ setMethod("spsample.prob", signature(observations = "SpatialPoints", covariates 
     dmap.max <- max(dmap@data[,1], na.rm=TRUE)
     dmap@data[,1] <- signif(dmap@data[,1]/dmap.max, 3)
     
-    ## inclusion probabilities in feature space:
+    ## occurrence probabilities in feature space:
     message("Deriving inclusion probabilities using MaxLike analysis...")
     fm <- as.formula(paste("~", paste(names(covariates), collapse="+")))
     ml <- maxlike::maxlike(formula=fm, rasters=stack(covariates), points=observations@coords, method="BFGS", savedata=TRUE)
@@ -37,7 +37,7 @@ setMethod("spsample.prob", signature(observations = "SpatialPoints", covariates 
     ml$call$formula <- fm
     ## TH: this operation can be time consuming and is not recommended for large grids!
     ml.p <- as(predict(ml), "SpatialPixelsDataFrame")
-    ## sum two inclusion probabilities (masks for the two maps need to be exactly the same):
+    ## sum two occurrence probabilities (masks for the two maps need to be exactly the same):
     covariates$iprob <- signif((ml.p@data[,1] + dmap@data[,1])/2, 3)
      
     out <- list(prob=covariates["iprob"], observations=as(observations, "SpatialPoints"), density=dmap, maxlike=ml.p, maxlikeFit=ml[-which(names(ml)=="rasters")])
