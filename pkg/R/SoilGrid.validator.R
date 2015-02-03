3ac18a322ecec906cc4c8f783a24cd48da752031
thengl <thengl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-03 18:13:01 +0000

    SoilGrid.validator.R fixes

--- pkg/R/SoilGrid.validator.R
+++ pkg/R/SoilGrid.validator.R
@@ -29,7 +29,7 @@ SoilGrid.validator <- function(obj, domain, ground.truth, N.sample=2000, xml.fil
     ll.x <- obj[["ll.x"]]
     ll.y <- obj[["ll.y"]]
   
-    ## C:MTD | Metadata file is available (in the same directory)?
+    ## C_MTD | Metadata file is available (in the same directory)?
     if(missing(xml.file)){ xml.file <- paste0(GDALobj.file, ".xml") }
     if(file.exists(xml.file)){
       message(paste("Reading the metadata file: ", xml.file, sep=""))
@@ -42,10 +42,10 @@ SoilGrid.validator <- function(obj, domain, ground.truth, N.sample=2000, xml.fil
         C_MTD <- NA
       }
     } else {
-      C_MTD <- "Missing or incorrect"
+      stop("Metadata file missing.")
     }
     
-    ## C:MTS | Metadata file passes schema validation (see INSPIRE metadata validator)?
+    ## C_MTS | Metadata file passes schema validation (see INSPIRE metadata validator)?
     if(md.type=="INSPIRE"){ 
       if(test.URL==TRUE){
         message("Connecting to the INSPIRE schema...")
@@ -64,8 +64,8 @@ SoilGrid.validator <- function(obj, domain, ground.truth, N.sample=2000, xml.fil
       }
     }
   
-    ## C:URL | All layers in the documentation are also available on the server (the URL in the metadata file exists)?
-    C_URL <- XML::xmlValue(ml[["distributionInfo"]][["MD_Distribution"]][["transferOptions"]][["MD_DigitalTransferOptions"]][["onLine"]][["CI_OnlineResource"]][["linkage"]][["URL"]])
+    ## C_URL | All layers in the documentation are also available on the server (the URL in the metadata file exists)?
+    try( C_URL <- XML::xmlValue(ml[["distributionInfo"]][["MD_Distribution"]][["transferOptions"]][["MD_DigitalTransferOptions"]][["onLine"]][["CI_OnlineResource"]][["linkage"]][["URL"]]) )
     if(!is.na(C_URL)&exists("C_URL")){
       if(RCurl::url.exists(C_URL)){
         ## test downloading the file:
@@ -85,17 +85,19 @@ SoilGrid.validator <- function(obj, domain, ground.truth, N.sample=2000, xml.fil
       download.time <- NA
     }
    
-    ## C:URD | URLs to find full data description have been provided in the Metadata file?
-    C_URD <- XML::xmlValue(ml[["idinfo"]][["citation"]][["citeinfo"]][["onlink"]])
+    ## C_URD | URLs to find full data description have been provided in the Metadata file?
+    try( C_URD <- XML::xmlValue(ml[["identificationInfo"]][["MD_DataIdentification"]][["pointOfContact"]][["CI_ResponsibleParty"]][["contactInfo"]][["CI_Contact"]][["onlineResource"]][["CI_OnlineResource"]][["linkage"]][[1]]) )
     if(!is.na(C_URD)&exists("C_URD")){
-      if(!RCurl::url.exists(C_URD)){
+      if(RCurl::url.exists(C_URD)){
+        C_URD <- "URL validated"
+      } else {
         C_URD <- "URL does not exist"
       }
     } else {
       C_URD <- "Missing URL in metadata file"
     }
   
-    ## C:SMK | Spatial predictions are available for at least 95% of the soil mask?
+    ## C_SMK | Spatial predictions are available for at least 95% of the soil mask?
     ri <- raster(GDALobj.file)
     if(fromDisk(ri)){
       rpnts <- spsample(SpatialGrid(GridTopology(c(ll.x,ll.y), c(res.x,res.y), c(columns,rows)), proj4string = CRS(prj4_string)), n=N.sample, type="random")
@@ -111,19 +113,19 @@ SoilGrid.validator <- function(obj, domain, ground.truth, N.sample=2000, xml.fil
       }
     }
     NA.mask <- !is.na(ov$d)
-    C_SMK <- round(sum(!is.na(ov$z[NA.mask]))/sum(NA.mask)*100)
-    mask.size <- round(sum(NA.mask)/length(NA.mask)*100)
+    C_SMK <- round(sum(!is.na(ov$z[NA.mask]))/sum(NA.mask)*100, 1)
+    mask.size <- round(sum(NA.mask)/length(NA.mask)*100, 1)
     
-    ## N:NRC | All rasters conform to the same grid (raster stack) i.e. all have exactly the same number of rows and columns, and the same coordinate system? 
+    ## N_NRC | All rasters conform to the same grid (raster stack) i.e. all have exactly the same number of rows and columns, and the same coordinate system? 
     if(any(!rows==domain[["rows"]]|!columns==domain[["columns"]]|!res.x==domain[["res.x"]]|!ll.x==domain[["ll.x"]]|!ll.y==domain[["ll.y"]]|!prj4_string==attr(domain, "projection"))){
       N_NRC <- "Inconsistent grid"
     } else {
       N_NRC <- "Consistent"
     }
     
-    ## N:PRJ | Proj4 string is provided (embedded in the GeoTIFF)? 
+    ## N_PRJ | Proj4 string is provided (embedded in the GeoTIFF)? 
     N_PRJ <- ifelse(prj4_string=="NA"|nchar(prj4_string)==0, "Missing", prj4_string)
-    ## N:PRS | Provided Proj4 string is standard - i.e. it is registered under some coordinate system registry e.g. http://www.prj2epsg.org/ (and available in different sofware formats)?
+    ## N_PRS | Provided Proj4 string is standard - i.e. it is registered under some coordinate system registry e.g. http://www.prj2epsg.org/ (and available in different sofware formats)?
     wkt <- showWKT(prj4_string)
     if(requireNamespace("rjson", quietly = TRUE)){
       if(test.URL==TRUE){
@@ -138,11 +140,11 @@ SoilGrid.validator <- function(obj, domain, ground.truth, N.sample=2000, xml.fil
       N_PRS <- "Not checked"
     }
   
-    ## N:LIM | For numeric variables - No spatial predictions outside of physical limits (upper and lower limits) e.g. pH < 2 or pH > 11? / For factor-type variables correspond to the original domain e.g. no predicted values outside the original legend?
+    ## N_LIM | For numeric variables - No spatial predictions outside of physical limits (upper and lower limits) e.g. pH < 2 or pH > 11? / For factor-type variables correspond to the original domain e.g. no predicted values outside the original legend?
     message("Deriving value range...")
     z.rn <- range(ov$z[NA.mask], na.rm=TRUE)
-    ## R:RME | RMSE i.e. Attribute Measurement Resolution or numeric resolution (RMSE/2) has been defined in the metadata file?
-    numeric.resolution <- as.numeric(XML::xmlValue(ml[["eainfo"]][["detailed"]][["attr"]][["attrdomv"]][["rdom"]][["attrmres"]]))
+    ## R_RME | RMSE i.e. Attribute Measurement Resolution or numeric resolution (RMSE/2) has been defined in the metadata file?
+    try( numeric.resolution <- as.numeric(XML::xmlValue(ml[["eainfo"]][["detailed"]][["attr"]][["attrdomv"]][["rdom"]][["attrmres"]])) )
     if(!is.na(numeric.resolution)&exists("numeric.resolution")){
       R_RME <- "RMSE defined in metadata"      
     } else {
@@ -155,13 +157,13 @@ SoilGrid.validator <- function(obj, domain, ground.truth, N.sample=2000, xml.fil
       N_LIM <- NA
     }
      
-    ## R:RMS | Spatial overlays using independent point data sets (at least 3 study areas / subsets representing the domain of interest) do not show RMSE > 2 times higher than the original RMSE i.e. four times the numeric resolution specified (extra analysis required)?
+    ## R_RMS | Spatial overlays using independent point data sets (at least 3 study areas / subsets representing the domain of interest) do not show RMSE > 2 times higher than the original RMSE i.e. four times the numeric resolution specified (extra analysis required)?
     if(!missing(ground.truth)){
       message("Testing RMSE using ground truth data (first column) ...")
       ov.cv <- data.frame(meas=ground.truth$z, pred=extract(x=ri, y=ground.truth))
       RMSE <- signif( sqrt(mean((ov.cv$meas - ov.cv$pred)^2, na.rm=TRUE)) , 3)
-      R_RMS <- ifelse(RMSE>4*numeric.resolution, "RMSE exceeds 4 x NR", "RMSE within limits")
-      ## N:HST | Histogram of the predicted variable corresponds to the sampled histogram (Chi-square test)?
+      R_RMS <- ifelse(RMSE>4*numeric.resolution, "RMSE exceeds 2 x declared RMSE", "RMSE within limits")
+      ## N_HST | Histogram of the predicted variable corresponds to the sampled histogram (Chi-square test)?
       hbb <- Hmisc::histbackback(ov.cv$meas, ov.cv$pred, prob=TRUE)
       ks <- ks.test(hbb$left, hbb$right)
       N_HST <- ifelse(ks$p.value < 0.05, "Two-sample Kolmogorov-Smirnov test failed", "No significant difference")
@@ -171,7 +173,7 @@ SoilGrid.validator <- function(obj, domain, ground.truth, N.sample=2000, xml.fil
       N_HST <- NA
     }
     
-    out <- data.frame(file.name=basename(GDALobj.file), prj4_string=N_PRJ, rows=rows, columns=columns, GDType=GDType, ll.x=ll.x, ll.y=ll.y, Z_MIN=signif(min(ov$z[NA.mask], na.rm=TRUE), 3), Z_MAX=signif(max(ov$z[NA.mask], na.rm=TRUE), 3), Z_STD=signif(sd(ov$z[NA.mask], na.rm=TRUE), 3), numeric.resolution=signif(numeric.resolution, 3), mask.size=mask.size, C_MTD=C_MTD, C_MTS=C_MTS, C_URL=C_URL, C_URD=C_URD, C_SMK=C_SMK, R_RME=R_RME, RMSE=RMSE, R_RMS=R_RMS, N_HST=N_HST, download.time=download.time)
+    out <- data.frame(file.name=basename(GDALobj.file), prj4_string=N_PRJ, rows=rows, columns=columns, GDType=GDType, ll.x=ll.x, ll.y=ll.y, Z_MIN=signif(min(ov$z[NA.mask], na.rm=TRUE), 3), Z_MAX=signif(max(ov$z[NA.mask], na.rm=TRUE), 3), Z_STD=signif(sd(ov$z[NA.mask], na.rm=TRUE), 3), numeric.resolution=signif(numeric.resolution, 3), N_LIM=N_LIM, mask.size=mask.size, C_SMK=C_SMK, C_MTD=C_MTD, C_MTS=C_MTS, C_URL=C_URL, C_URD=C_URD, R_RME=R_RME, RMSE=RMSE, R_RMS=R_RMS, N_HST=N_HST, download.time=download.time)
     message("Finished!")
     return(out)
   }
